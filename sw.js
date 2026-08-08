ffae3c19f75ea94d34b2e5de663077a877ef06a1
jhlustig <jhlustig@mac.com>
2026-08-08 09:43:41 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'reba-08-08·4de';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-180.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request, {ignoreSearch:true}).then(hit => hit || fetch(e.request).then(r => {
+    if (r.ok && e.request.method === 'GET') { const cp = r.clone(); caches.open(CACHE).then(c => c.put(e.request, cp)); }
+    return r;
+  }).catch(() => caches.match('./index.html'))));
+});
